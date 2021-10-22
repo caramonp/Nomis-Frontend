f2dda85f34469ed681ad2d9163f3567787b4d66e
daniapm <daniajibellis@hotmail.com>
2021-10-22 15:30:01 -0500

    #9-agregando componente status

---
+++ front-tributi/src/components/Status/Status.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import firestore from '../../config/firebase';
+import 'firebase/firestore'
+import 'firebase/compat/firestore';
+
+const Status = () => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		firestore.collection('books')
+			.get()
+			.then(querySnapshot => {
+				const books = [];
+				querySnapshot.forEach(doc => {
+					books.push(doc.data());
+				});
+				setData([...books]);
+			});
+	}, []);
+
+		return (<div>
+			<h1>lista de libros</h1>
+			{data.length ? (
+				data.map(book => (
+					<article key={book.id}>
+						<h2>{book.tittle}</h2>
+						<h3>{book.author}</h3>
+					</article>
+				))
+			) : (
+				<p>Cargando Coleccion</p>
+			)}
+		</div>
+		);
+	}
+export default Status;

d42f27b0f5d5d7f683a7605b2879f7c6b672b202
daniapm <daniajibellis@hotmail.com>
2021-10-27 15:57:53 -0500

    Componente Status - Extrayendo datos de Firebase

--- front-tributi/src/components/Status/Status.jsx
+++ front-tributi/src/components/Status/Status.jsx
@@ -1,65 +1,26 @@
-import { Steps, Button, message } from 'antd';
-import React from 'react'
-import AddingCollection from './AddingCollection';
-import ShowCollection from './ShowCollection';
+import { Progress } from 'antd';
+import React, { useState, useEffect}from 'react'
+import firestore from '../../config/firebase';
+import 'firebase/firestore'
+import 'firebase/compat/firestore';
+import './Status.css'
 
-const { Step } = Steps;
 
+export const Status = (props) => {
 
+  const [docs, setdocs] = useState([])
 
-const steps = [
-  {
-    title: 'Agregando Documentos',
-    content: <AddingCollection/>,
-  },
-  {
-    title: 'Second',
-    content: <ShowCollection/>,
-  },
-  {
-    title: 'Last',
-    content: 'Last-content',
-  },
-];
-
-const StatusComponent = () => {
-  const [current, setCurrent] = React.useState(0);
-
-  const next = () => {
-    setCurrent(current + 1);
-  };
-
-  const prev = () => {
-    setCurrent(current - 1);
-  };
-
-  return (
-    <>
-      <Steps current={current}>
-        {steps.map(item => (
-          <Step key={item.title} title={item.title} />
-        ))}
-      </Steps>
-      <div className="steps-content">{steps[current].content}</div>
-      <div className="steps-action">
-        {current < steps.length - 1 && (
-          <Button type="primary" onClick={() => next()}>
-            Next
-          </Button>
-        )}
-        {current === steps.length - 1 && (
-          <Button type="primary" onClick={() => message.success('Processing complete!')}>
-            Done
-          </Button>
-        )}
-        {current > 0 && (
-          <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-            Previous
-          </Button>
-        )}
-      </div>
-    </>
-  );
-};
-
-export default StatusComponent;
+  const getDocument = async () => {
+    firestore.collection('Prueba_nomis').onSnapshot((querySnapshot) => {
+      const doct = []
+      querySnapshot.forEach(doc => {
+        doct.push({ ...doc.data(), id: doc.id })
+      });
+      setdocs(doct)
+    })
+  }
+  useEffect(() => {
+    getDocument()
+		
+  }, []);
+}

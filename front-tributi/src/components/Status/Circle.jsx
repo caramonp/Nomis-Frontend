f620d02c090bd9a571520a8f545370db2dd61747
daniapm <daniajibellis@hotmail.com>
2021-10-27 22:59:57 -0500

    Componente Status - Configurando estilo

--- front-tributi/src/components/Status/Circle.jsx
+++ front-tributi/src/components/Status/Circle.jsx
@@ -26,62 +26,62 @@ const DefaultSpinner = styled.div`
   div:nth-child(1) {
     animation-delay: 0s;
     top: 29px;
-    left: 690px;
+    left: 677px;
   }
   div:nth-child(2) {
     animation-delay: -0.1s;
     top: 18px;
-    left: 687px;
+    left: 673px;
   }
   div:nth-child(3) {
     animation-delay: -0.2s;
     top: 9px;
-    left: 678px;
+    left: 664px;
   }
   div:nth-child(4) {
     animation-delay: -0.3s;
     top: 6px;
-    left: 669px;
+    left: 655px;
   }
   div:nth-child(5) {
     animation-delay: -0.4s;
     top: 9px;
-    left: 658px;
+    left: 645px;
   }
   div:nth-child(6) {
     animation-delay: -0.5s;
     top: 18px;
-    left: 649px;
+    left: 636px;
   }
   div:nth-child(7) {
     animation-delay: -0.6s;
     top: 29px;
-    left: 646px;
+    left: 633px;
   }
   div:nth-child(8) {
     animation-delay: -0.7s;
     top: 41px;
-    left: 649px;
+    left: 636px;
   }
   div:nth-child(9) {
     animation-delay: -0.8s;
     top: 50px;
-    left: 656px;
+    left: 643px;
   }
   div:nth-child(10) {
     animation-delay: -0.9s;
     top: 53px;
-    left: 666px;
+    left: 653px;
   }
   div:nth-child(11) {
     animation-delay: -1s;
     top: 50px;
-    left: 678px;
+    left: 665px;
   }
   div:nth-child(12) {
     animation-delay: -1.1s;
     top: 41px;
-    left: 686px;
+    left: 673px;
   }
 `
 
--- front-tributi/src/components/Status/Status.jsx
+++ front-tributi/src/components/Status/Status.jsx
@@ -26,24 +26,17 @@ export const Status = (props) => {
   }, []);
 
   const [valueEnd, setValueEnd] = React.useState(1);
-  let file = <Progress type="dashboard" percent={valueEnd} status="active" gapDegree={0} format={() => 'Update File'} showInfo={true}/>
   let dumps = <Progress type="dashboard" percent={valueEnd} status="active" gapDegree={0} format={() => 'Update Dumps'} />
   let mathops = <Progress type="dashboard" percent={valueEnd} status="active" gapDegree={0} format={() => 'Mathops'} />
-  let ligero = <Progress type="dashboard" percent={valueEnd} status="active" gapDegree={0} format={() => 'File Ligero'} />
+  let ligero = <Progress type="dashboard" percent={valueEnd} status="active" gapDegree={0} format={() => 'Lightweight File'} />
 
 
     return (
       <div className="Status"> 
         <h1 className="title-Status"> Api Status</h1>
         {docs.map(dc => (
-          <div className="Acceso-datos"key={dc.id}>
-            {(() => {
-              if (dc.FILE === 'SI'){
-                  return (
-                      file
-                  )
-              }
-            })()}
+          <div className="Acceso-datos" key={dc.id}>
+            <Progress type="dashboard" percent={valueEnd} status="active" gapDegree={0} format={() => 'Update File'} showInfo={true}/>
             {(() => {
               if (dc.DUMP === 'SI'){
                   return (
@@ -65,6 +58,7 @@ export const Status = (props) => {
                   )
               }
             })()}
+            <Progress type="dashboard" percent={valueEnd} status="active" gapDegree={0} format={() => 'Done'} />
              
             </div>
         ))}
@@ -74,7 +68,7 @@ export const Status = (props) => {
         <br />
         <br />
         
-        <button onClick={() => setValueEnd(valueEnd + 10)}>Change valueEnd</button>
+        <button className="Button-Start" onClick={() => setValueEnd(valueEnd + 10)}>Start</button>
         <br />
         <div>
         <Circle/>

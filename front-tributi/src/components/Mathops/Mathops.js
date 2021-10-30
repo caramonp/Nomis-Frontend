f02c2b3bbf37e914f4ec99effb2cd4e6e462df04
daniapm <daniajibellis@hotmail.com>
2021-10-30 15:58:52 -0500

    Componente Mathops - Configurando useContext

--- front-tributi/src/components/Mathops/Mathops.js
+++ front-tributi/src/components/Mathops/Mathops.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useContext } from "react";
 import 'antd/dist/antd.css';
-import './Mathops.css';
+import {Button} from 'antd';
 import SelectButton from './SelectButton';
+import Context from '../../context';
+import './Mathops.css';
 
 const MathopsComponent = () => {
+  const [mathops, setMathops] = useState();
+  const context = useContext(Context)
+  const { state, setState } = context;
+
+  const handleClick = (e) => {
+    console.log(mathops);
+    console.log("state", state)
+    setState({
+      ...state,
+      mathops: {
+        mathops: JSON.parse(mathops),
+      }
+    })
+  };
+
   return (
     <div className="box-dragg-file">
       <section className="title-update-dump">
@@ -13,8 +30,9 @@ const MathopsComponent = () => {
       </section>
       <h2>¿Generaste Mathops?</h2>
       <div className="select-button">
-        <SelectButton />
+        <SelectButton setValue={setMathops} />
       </div>
+      <Button className="select-button-mathops" style={{ background: '#2a6662', border: '#2a6662'}} type="primary" onClick={handleClick}>Guardar</Button>
     </div>
   )
 }

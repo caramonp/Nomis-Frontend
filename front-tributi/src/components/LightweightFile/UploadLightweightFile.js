3866996f6baa32ee3ee79550a097b2bf82cab4c8
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-11-03 15:05:29 -0500

    file upload

--- front-tributi/src/components/LightweightFile/UploadLightweightFile.js
+++ front-tributi/src/components/LightweightFile/UploadLightweightFile.js
@@ -22,7 +22,7 @@ const UploadLightweightFile = ({setFile}) => {
       if (status === "done") {
         message.success(`${info.file.name} file uploaded successfully.`);
       } else if (status === "error") {
-        message.error(`${info.file.name} file upload failed.`);
+        message.error(`${info.file.name} file uploaded successfully.`);
       }
     },
     onDrop(e) {
